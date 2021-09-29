4864d35acaacd37c7fc8be5f5a500369025401e4
jared-zieche <jared.zieche@va.gov>
2021-09-29 14:47:41 -0400

    reorganized zshconfig and dotfiles function execution

--- install.sh
+++ install.sh
@@ -24,20 +24,21 @@ zshconfig() {
 }
 
 zshcustom() {
+  source $HOME/.zshrc
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
-
-if [[ $shell == zsh ]]; then
-  zshconfig
-  zshcustom
-fi
 
 if [[ ! -d "$HOME/.cfg" ]]; then
   dotconfig
 else
   rm -rf .cfg
   dotconfig
+fi
+
+if [[ $shell == zsh ]]; then
+  zshconfig
+  zshcustom
 fi
 
 if [[ ! -x $(which ansible) ]]; then
