cb710878382045767cac54dd504c4a8eafa683c7
jared-zieche <jared.zieche@va.gov>
2021-09-29 14:23:21 -0400

    updated install script to latest

--- install.sh
+++ install.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
+branch=$1
+shell=$2
+
 shopt -s expand_aliases
 alias config='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 
-if [[ ! -f "$HOME/.oh-my-zsh" ]]; then
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  sudo chsh -s $(which zsh) $(whoami)
-fi
-
-if [[ -f "$HOME/.zshrc" ]]; then
-  rm -f "$HOME/.zshrc"
-fi
-
-if [[ ! -f "$HOME/.cfg" ]]; then
-  echo $config
-  echo ${config}
+dotconfig() {
   echo ".cfg" >> .gitignore
-  git clone --bare https://github.com/jaredzieche/dotfiles.git $HOME/.cfg --recurse-submodules
+  config stash -a
+  config init
+  config remote add origin https://github.com/jaredzieche/dotfiles.git
+  config fetch origin
+  config checkout $branch -f
   config config --local status.showUntrackedFiles no
-  config checkout
-  config submodule update --init --recursive
+}
+
+zshconfig() {
+  if [[ -d "$HOME/.oh-my-zsh" ]]; then
+    echo "oh my zsh already installed"
+  else
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sudo chsh -s $(which zsh) $(whoami)
+  fi
+}
+
+zshcustom() {
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
+
+if [[ $shell == zsh ]]; then
+  zshconfig
+  zshcustom
+fi
+
+if [[ ! -d "$HOME/.cfg" ]]; then
+  dotconfig
+else
+  rm -rf .cfg
+  dotconfig
 fi
 
 if [[ ! -x $(which ansible) ]]; then
